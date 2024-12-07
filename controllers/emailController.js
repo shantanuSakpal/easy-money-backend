1c646ed1fa9447bdba8c1cce364dfe79b41e2590
shantanuSakpal <shantanuesakpal1420@gmail.com>
2024-12-07 15:21:25 +0530

    render issue

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -69,15 +69,15 @@ const sendInvoiceEmail = async (req, res) => {
     }
 
     // Generate PDF with Puppeteer
-    // browser = await puppeteer.launch({
-    //   headless: "new",
-    //   args: ["--no-sandbox"],
-    // });
-
     browser = await puppeteer.launch({
-      executablePath: "/opt/render/.cache/puppeteer/chrome-linux/chrome",
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: "new",
+      args: ["--no-sandbox"],
     });
+
+    // browser = await puppeteer.launch({
+    //   executablePath: "/opt/render/.cache/puppeteer/chrome-linux/chrome",
+    //   args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    // });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: "networkidle0" });
 
